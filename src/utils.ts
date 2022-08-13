e163cd166530f4e7de8ec4e17ffb32b2b6eb38d0
iwillwen <willwengunn@gmail.com>
2022-08-14 05:05:56 +0800

    feat: test within timeout

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import type { NextApiResponse } from "next";
 import qs from "query-string";
 
 export function formatApiUrl(
@@ -6,4 +7,26 @@ export function formatApiUrl(
   query: Record<string, any>
 ) {
   return new URL(path + "?" + qs.stringify(query), base).toString();
+}
+
+export async function within<T extends (...args: unknown[]) => unknown>(
+  fn: T,
+  res: NextApiResponse,
+  duration: number
+) {
+  const id = setTimeout(
+    () =>
+      res.json({
+        message: "There was an error with the upstream service!",
+      }),
+    duration
+  );
+
+  try {
+    let data = await fn();
+    clearTimeout(id);
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
 }
